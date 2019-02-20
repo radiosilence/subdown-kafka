5dcc509d6408994f2d6b109f7a56e5d40aa82d4f
James Cleveland <jc@blit.cc>
2019-02-20 00:14:46 +0000

    spider now receives messages

--- src/main/java/download/DownloadSerde.kt
+++ src/main/java/download/DownloadSerde.kt
@@ -1,12 +1,12 @@
-package spider
-
+package download
 
 import api.jsonMapper
 import org.apache.kafka.common.serialization.Deserializer
 import org.apache.kafka.common.serialization.Serializer
 
-class SpiderSerializer : Serializer<Spider> {
-    override fun serialize(topic: String, data: Spider?): ByteArray? {
+
+class DownloadSerializer : Serializer<Download> {
+    override fun serialize(topic: String, data: Download?): ByteArray? {
         if (data == null) return null
         return jsonMapper.writeValueAsBytes(data)
     }
@@ -15,10 +15,10 @@ class SpiderSerializer : Serializer<Spider> {
     override fun configure(configs: MutableMap<String, *>?, isKey: Boolean) {}
 }
 
-class SpiderDeserializer : Deserializer<Spider> {
-    override fun deserialize(topic: String, data: ByteArray?): Spider? {
+class DownloadDeserializer : Deserializer<Download> {
+    override fun deserialize(topic: String, data: ByteArray?): Download? {
         if (data == null) return null
-        return jsonMapper.readValue(data, Spider::class.java)
+        return jsonMapper.readValue(data, Download::class.java)
     }
 
     override fun close() {}
--- src/main/java/spider/Main.kt
+++ src/main/java/spider/Main.kt
@@ -1,2 +1,71 @@
 package spider
 
+import api.spiderTopic
+import download.Download
+import download.DownloadSerializer
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.log4j.LogManager
+import java.time.Duration
+import java.util.*
+
+fun main(args: Array<String>) {
+    SpiderProcessor("localhost:9092").process()
+}
+
+class SpiderProcessor(brokers: String) {
+    private val logger = LogManager.getLogger(javaClass)
+    private val consumer = createConsumer(brokers)
+    private val spiderProducer = createSpiderProducer(brokers)
+    private val downloadProducer = createDownloadProducer(brokers)
+
+    fun process() {
+        consumer.subscribe(listOf(spiderTopic))
+
+        while (true) {
+            val records = consumer.poll(Duration.ofSeconds(1))
+            logger.info("Received ${records.count()} spiders")
+
+            records.iterator().forEach {
+                val spider = it.value()
+                logger.info("SPIDER! $spider")
+            }
+        }
+    }
+
+    private fun createConsumer(brokers: String): Consumer<String, Spider> {
+        val props = Properties()
+
+        props["bootstrap.servers"] = brokers
+        props["group.id"] = "spider-processor"
+        props["key.deserializer"] = StringDeserializer::class.java
+        props["value.deserializer"] = SpiderDeserializer::class.java
+
+        return KafkaConsumer<String, Spider>(props)
+    }
+
+    private fun createSpiderProducer(brokers: String): Producer<String, Spider> {
+        val props = Properties()
+
+        props["bootstrap.servers"] = brokers
+        props["key.serializer"] = StringSerializer::class.java
+        props["value.serializer"] = SpiderSerializer::class.java
+
+        return KafkaProducer<String, Spider>(props)
+    }
+
+    private fun createDownloadProducer(brokers: String): Producer<String, Download> {
+        val props = Properties()
+
+        props["bootstrap.servers"] = brokers
+        props["key.serializer"] = StringSerializer::class.java
+        props["value.serializer"] = DownloadSerializer::class.java
+
+        return KafkaProducer<String, Download>(props)
+    }
+
+}
